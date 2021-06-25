ddddafddb57df9dec068ff86d9477baf6eba5357
Pablo Felipe Domingos De Paiva <84213444+pablofpaiva@users.noreply.github.com>
2021-06-24 23:21:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemySplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            String[] v = s.Split(' ');
+
+            
+
+            string a = v[0];
+
+            string b = v[1];
+
+            string c = v[2];
+
+            Console.WriteLine("vc digitou" + v);
+        }
+    }
+}
